fa91f277cdeb8bdf835f8b8662005db4828c4d76
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-10-29 09:50:55 -0700

    refactors business logic to use a loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
 function pathfinder(userInput) {
   let functionInput = userInput.toString();
-  if (functionInput.includes(3)) {
-    return 3;
-  }else if (functionInput.includes(2)) {
-    return 2;
-  }else if (functionInput.includes(1)) {
-    return 1;
-  }else{
-    return userInput;
+  for (let i = 3; i>0; i--){
+  if (functionInput.includes(i)) {
+    return i;
   }
+}
+return userInput;
 }
