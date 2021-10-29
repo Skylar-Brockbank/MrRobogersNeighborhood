d79075faf72ccc97e9be5d62e78f48467fc2654c
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-10-29 10:20:01 -0700

    basic UI logic roughed in

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//business logic
 function pathfinder(userInput) {
   let functionInput = userInput.toString();
   for (let i = 3; i>0; i--){
@@ -7,3 +8,21 @@ function pathfinder(userInput) {
 }
 return userInput;
 }
+
+//user interface logic
+
+function returnFormattedValues(number) {
+  let comparison = ["Beep!","Boop!","Won't you be my neighbor?"]
+  if((number-1)<comparison.length){
+    $("#outputs").prepend("<li>" + comparison[number-1] + "</li>");
+  }
+}
+
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+    let input = pathfinder($("#userInputBox").val())
+    returnFormattedValues(input);
+    this.reset();
+  });
+});
