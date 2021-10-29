52de08efef3f8d89ab6d2c6a0da9a271793ddc4d
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-10-29 08:56:44 -0700

    make test 4 pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 function pathfinder(userInput) {
   let functionInput = userInput.toString();
-  if (functionInput.includes(2)) {
+  if (functionInput.includes(3)) {
+    return 3;
+  }else if (functionInput.includes(2)) {
     return 2;
   }else if (functionInput.includes(1)) {
     return 1;
